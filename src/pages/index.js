234093bc70357b5e4ac13ace482a3b02686376fb
Yaroslaw Bagriy <yarslaw.bagriy@evercode.io>
2020-08-15 10:55:37 -0500

    change newsletter import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Layout from "../components/layout";
-import Newsletter from "../components/Newsletter";
+import Newsletter from "../components/newsletter";
 import SEO from "../components/seo";
 
 const Hero = () => (
